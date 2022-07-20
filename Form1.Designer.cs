086006f1edda717b1a816aa1a4993b23d0c38a26
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-07-20 07:38:24 -0600

    feat: Completed TreeView exercise

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,14 +30,30 @@ namespace CheckListBoxTest
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Original ");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("The Next Generation");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Voyager");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Deep Space Nine");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Enterprise");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Discovery");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Strange New Worlds");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Star Trek", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6,
+            treeNode7});
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.displaylistBox = new System.Windows.Forms.ListBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.addButton = new System.Windows.Forms.Button();
-            this.inputTextBox = new System.Windows.Forms.TextBox();
             this.removeButton = new System.Windows.Forms.Button();
             this.clearButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
             // checkedListBox1
@@ -81,13 +97,6 @@ namespace CheckListBoxTest
             this.addButton.UseVisualStyleBackColor = true;
             this.addButton.Click += new System.EventHandler(this.addButton_Click);
             // 
-            // inputTextBox
-            // 
-            this.inputTextBox.Location = new System.Drawing.Point(589, 13);
-            this.inputTextBox.Name = "inputTextBox";
-            this.inputTextBox.Size = new System.Drawing.Size(199, 20);
-            this.inputTextBox.TabIndex = 3;
-            // 
             // removeButton
             // 
             this.removeButton.Location = new System.Drawing.Point(589, 94);
@@ -121,11 +130,45 @@ namespace CheckListBoxTest
             this.exitButton.UseVisualStyleBackColor = true;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(589, 13);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(199, 20);
+            this.inputTextBox.TabIndex = 3;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(589, 185);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Original";
+            treeNode1.Text = "Original ";
+            treeNode2.Name = "TheNextGeneration";
+            treeNode2.Text = "The Next Generation";
+            treeNode3.Name = "Voyager";
+            treeNode3.Text = "Voyager";
+            treeNode4.Name = "DeepSpaceNine";
+            treeNode4.Text = "Deep Space Nine";
+            treeNode5.Name = "Enterprise";
+            treeNode5.Text = "Enterprise";
+            treeNode6.Name = "Discovery";
+            treeNode6.Text = "Discovery";
+            treeNode7.Name = "StrangeNewWorlds";
+            treeNode7.Text = "Strange New Worlds";
+            treeNode8.Name = "StarTrekNode";
+            treeNode8.Text = "Star Trek";
+            treeNode8.ToolTipText = "All Star Trek series fall below ";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode8});
+            this.treeView1.Size = new System.Drawing.Size(199, 248);
+            this.treeView1.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.clearButton);
             this.Controls.Add(this.removeButton);
@@ -150,6 +193,7 @@ namespace CheckListBoxTest
         private System.Windows.Forms.Button removeButton;
         private System.Windows.Forms.Button exitButton;
         protected System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
 
